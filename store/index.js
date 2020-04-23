a161c1fc6ab3717eb45b78e1748f353b15469336
Shubhi Thakuria <shubhi_thakuria@berkeley.edu>
2020-04-23 15:38:34 -0700

    betterpic

--- store/index.js
+++ store/index.js
@@ -2,8 +2,8 @@ export const state = () => {
   return { count: 3,allowedUrls:["how-to-set-up-responsive-ui-search-in-vue-js","vue-blog"],stuff:[
     {
       id: 1,
-      title: "Inclusive Use of Broadband Connectivity for Quality Education",
-      objective: "X",
+      title: "Inclusive Broadband Connectivity",
+      objective: "Created policy advocacy content for 50+ Asia Pacific countries",
       url:
         "https://www.unescap.org/sites/default/files/Inclusive%20Use%20of%20Broadband%20Connectivity%20for%20Quality%20Education%2C%20Insights%20from%20Asia%20and%20the%20Pacific_0.pdf",
       publication: "United Nations ESCAP",
@@ -13,89 +13,43 @@ export const state = () => {
     },
     {
       id: 2,
-      title: "Business and Social Opportunity for E-Learning in Indian Agriculture",
-      objective: "X",
-      url:
-        "https://drive.google.com/open?id=1VwY-sJUGCg3zt4dHwrBRXSmT5eh9Xv8T",
-      publication: "Coursera",
-      Year: "2019",
-      imageUrl:
-        "Coursera.png"
-    },
-    {
-      id: 3,
-      title:"Physics and Technology for Future Presidents",
-      objective: "X",
-      url:
-        "https://drive.google.com/drive/folders/1fj5rJhsS10e5dW8fALhTF2AsBrX-s44z?usp=sharing",
-      publication: "University of California, Berkeley",
-      Year: "2018",
-      imageUrl:
-        "P10.png"
-    },
-    {
-      id: 4,
-      title: "IIMA’s Digital Leap: Formulating Online Education Strategy",
-      objective: "X",
-      url:
-        "https://cases.iima.ac.in/index.php/iima-s-digital-leap-formulating-online-education-strategy.html",
-      publication: "Indian Institute of Management, Ahmedabad",
-      Year: "2019",
-      imageUrl:
-        "IIMA.png"
-    },
-    {
-      id: 5,
-      title:"Okhai Center for Empowerment: Refashioning the lives of rural women",
-      objective: "X",
-      url:
-        "https://cases.iima.ac.in/index.php/okhai-centre-for-empowerment-refashioning-the-lives-of-rural-women.html",
-      publication: "Okhai Enterprise ",
-      Year: "2017",
-      imageUrl:
-        "Okhai.png"
-    },
-
-    {
-      id: 6,
       title:"Salesforce training material",
-      objective: "X",
+      objective: "Created training modules and trained employees to use Salesforce features ",
       url:
-        "",
+        "Confidential material",
       publication: "Room To Read",
       Year: "2020",
       imageUrl:
-        "sales.png"
+        "SF1.png"
     },
-
-
+    
     {
-      id: 7,
+      id: 3,
       title:
-       "Grihini Social Welfare Society: Breaking Stereotypes Empowering Women",
-       objective: "X",
+       "Social Welfare Society: Empowering Women",
+       objective: "Create social sector enterprise scaling strategy training material for corporate social responsibility professionals. Won Best Case Award from International Management Research Academy, London.",
        url:
-         "https://www.researchgate.net/publication/330145312_Tata_Motors_Grihini_Social_Welfare_Society_'Breaking_Stereotypes_Empowering_Women",
-      publication: "Tata Motors Limited",
+         "https://drive.google.com/open?id=1ftjbr4B4YgbacxewpOARLlu3Ox2kbYk3",
+      publication: "Grihini, Tata Motors Limited",
       Year: "2015",
       imageUrl:
-        "Grihini.png"
-    },
-
+        "GH1.png" 
+      },
+    
+      {
+        id: 4,
+        title:
+          "Club Enerji Program",
+        objective: "Created energy conservation training material for environment professionals and school leadership. Reached 450+ Indian schools.",
+        url:
+           "https://drive.google.com/open?id=1SfOLcYTDFg3R0tf0QyBiptHJNc586Xdm",
+        publication: "Tata Power Limited",
+        Year: "2016",
+        imageUrl:
+          "CEE1.png"
+      },
     {
-      id: 8,
-      title:
-        "Club Enerji Program",
-      objective: "X",
-      url:
-         "https://www.researchgate.net/publication/330145381_Club_Enerji_Program_at_Tata_Power",
-      publication: "Tata Power Limited",
-      Year: "2016",
-      imageUrl:
-        "CE.png"
-    },
-    {
-      id: 9,
+      id: 5,
       title:
         "SauMill: The Journey of Cost & Value Innovation at Tata Communications Limited",
       objective:"X",
@@ -105,6 +59,55 @@ export const state = () => {
       Year: "2017",
       imageUrl:
         "SAU.png"
+    },
+
+    {
+      id: 6,
+      title:"Physics and Technology for Future Presidents",
+      objective: "Taught 170+ undergraduate students and created classroom teaching and course assessment for teaching Physics.",
+      url:
+        "https://drive.google.com/drive/folders/1TrusSz07Jn0U1euX9QX_WDaAT-WnIxVX?usp=sharing",
+      publication: "University of California, Berkeley",
+      Year: "2018",
+      imageUrl:
+        "QC1.png"
+    },
+
+    {
+      id: 8,
+      title: " Agriculture E-Learning",
+      objective: "Created instruction development framework for e-learning with potential to impact 100 Mn farmers.",
+      url:
+        "https://drive.google.com/file/d/1qz3ea8c6C57pBCDN9g0Y38xNkdVvfDZi/view?usp=sharing",
+      publication: "Coursera",
+      Year: "2019",
+      imageUrl:
+        "CS1.png"
+    },
+
+
+    {
+      id: 8,
+      title: "IIMA’s Digital Leap: Online Education Strategy",
+      objective: "Create training material for e-learning business development; resulted in launch of online courses earning $X Mn annually",
+      url:
+        "https://cases.iima.ac.in/index.php/iima-s-digital-leap-formulating-online-education-strategy.html",
+      publication: "Indian Institute of Management, Ahmedabad",
+      Year: "2019",
+      imageUrl:
+        "IIMA1.png"
+    },
+
+    {
+      id: 9,
+      title:"Okhai Center for Empowerment: Refashioning lives",
+      objective: "Created training material for a Handicraft Organization",
+      url:
+        "https://cases.iima.ac.in/index.php/okhai-centre-for-empowerment-refashioning-the-lives-of-rural-women.html",
+      publication: "Okhai Enterprise ",
+      Year: "2017",
+      imageUrl:
+        "Okhai.png"
     },
     {
       id: 10,
