0629e7f36de88424131610f4e55bb8667b1297a0
Shubhi Thakuria <shubhi_thakuria@berkeley.edu>
2020-07-06 16:11:19 -0700

    Changes to code

--- store/index.js
+++ store/index.js
@@ -4,7 +4,7 @@ export const state = () => {
     {
       id: 1,
       title:"Physics and Technology for Future Presidents",
-      objective: "Created course prototype, interactive quiz, taught 200+ students (Canvas LMS, Storyline)",
+      objective: "Course prototype, taught 200+ students, created interactive quiz and games (Canvas LMS, Storyline)",
       url:
         "https://360.articulate.com/review/content/15915d84-7b73-4221-b60e-c07ca206bc89/review",
       publication: "University of California, Berkeley, USA",
@@ -15,8 +15,8 @@ export const state = () => {
 
     {
       id: 2,
-      title: "Broadband Connectivity for Quality Education",
-      objective: "Created e-learning policy advocacy content for 50+ Asia Pacific countries.",
+      title: "Inclusive Broadband for Quality Education",
+      objective: "E-learning policy content for 50+ Asia Pacific countries.",
       url:
         "https://drive.google.com/drive/folders/1rT5H4y7p7AcQtVePONcVSy_N0a1gSPPX?usp=sharing",
       publication: "United Nations ESCAP, Bangkok, Thailand",
@@ -29,7 +29,7 @@ export const state = () => {
     {
       id: 3,
       title:"Refashoning Lives of Rural Women Artisans",
-      objective: "Co-authored case study with Branched Scenario's for Handicraft industry professionals",
+      objective: "Case study with branched scenario's for handicraft industry professionals",
       url:
         "https://drive.google.com/drive/folders/19c8QXekIMiYDUY6QORaVsiCIL-pZHHo9?usp=sharing",
       publication: "Okhai Enterprise, Gujarat, India",
@@ -42,7 +42,7 @@ export const state = () => {
       id: 4,
       title:
         "Greenolution: Environmental Conservation",
-      objective: "Created interaction and case study on workplace sustainability (Storyline)",
+      objective: "Case study and layers interaction for sustainability professionals (Articulate)",
       url:
          "https://360.articulate.com/review/content/04a96cae-cfa1-4480-ad76-84d2a53636b0/review",
       publication: "Tata Power Limited, Mumbai, India",
@@ -55,7 +55,7 @@ export const state = () => {
       id: 5,
       title:
         "Club Enerji Sustainability Education",
-      objective: "Rise 360 course for sustainability professionals & school leaders. Reached 450+ schools",
+      objective: "Energy conservation course for school leadership (Rise 360). Reached 450+ schools ",
       url:
          "https://rise.articulate.com/share/1zMEPKU_MLgnNKtWMAlU8yZDiAxRQpM0",
       publication: "Tata Power, Mumbai, India",
@@ -68,7 +68,7 @@ export const state = () => {
       id: 6,
       title:
        "Life Skills for Women Leaders",
-       objective: "Microlearning Animation (Biteable). Case received Best Case Award from International Management Research Academy, London ",
+       objective: "Microlearning animation (Biteable). Case received Best Case Award from International Management Research Academy, London ",
        url:
          "https://biteable.com/watch/understanding-boundary-through-animation-micro-l-2596181",
       publication: "Grihini, Tata Motors Limited, Pune, India",
@@ -80,9 +80,8 @@ export const state = () => {
       {
         id: 7,
         title:"Salesforce Training",
-        objective: "Created branched scenario's; trained staff in Europe, Asia, and Australia (Adobe Captivate)",
-        url:
-        "https://360.articulate.com/review/content/3c822b98-f055-42ae-b127-5abad421bef8/review",
+        objective: "Branched scenario, software simulation, job aids, and interactive screenshots; trained staff in Europe, Asia, and Australia (Adobe Captivate)",
+        url:"https://360.articulate.com/review/content/b0ff34a5-8707-49c6-bf5f-10a1ee0ec8a3/review",
         publication: "Room To Read, San Francisco, USA",
         Year: "2020",
         imageUrl:
@@ -93,7 +92,7 @@ export const state = () => {
     {
       id: 8,
       title: " E-Learning in Agriculture ",
-      objective: "Created content and business development framework - to reach a new market of ~100 Mn people",
+      objective: "Content and business development framework for agri-ed-tech - to reach a new market of ~100 Mn people",
       url:
         "https://www.slideshare.net/secret/EGBdN99RltKLkw",
       publication: "Coursera, Mountain View, USA",
@@ -105,8 +104,8 @@ export const state = () => {
     {
       id: 9,
       title:
-        "Skilling Truck Drivers",
-      objective: "Created training material and teaching notes for employee training (Vyond)",
+        "Professional Driver Training Initiative",
+      objective: "Training material for sales professionals (Vyond)",
       url:
          "https://drive.google.com/drive/folders/1cy2P9xkNe3IS7Ln6RjR0NmT46i24oOAP?usp=sharing",
       publication: "Tata Motors Limited, Pune, India",
@@ -118,8 +117,8 @@ export const state = () => {
     {
       id: 10,
       title:
-        "SauMill: Cost & Value Innovation",
-      objective:"Created case study and interactions on innovation (Storyline)",
+        "Design Thinking for Cost & Value Innovation",
+      objective:"Case study and dial interactions on innovation (Storyline)",
       url:
          "https://360.articulate.com/review/content/d1e4b017-6cb2-44e8-8b5b-5ae49a4ec61a/review",
       publication: "Tata Communication Limited, Mumbai, India",
@@ -131,7 +130,7 @@ export const state = () => {
     {
       id: 11,
       title: "Online Education Business Development",
-      objective: "Ed-tech case study for postgraduates with teaching notes; program earns ~$2 Mn annually.",
+      objective: "Ed-tech case study for postgraduates and executives; program earns ~$2 Mn annually.",
       url:
         "https://drive.google.com/drive/folders/1-COJKyPPwn4lvu5dnr-so1HZt0LEt7TT?usp=sharing",
       publication: "Indian Institute of Management Ahd, India",
